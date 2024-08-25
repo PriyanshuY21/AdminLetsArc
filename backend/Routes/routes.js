3791ea19b4dc14630b4ff9e638f698bded83d7ab
Priyanshu <106882732+PriyanshuY21@users.noreply.github.com>
2024-08-25 11:49:37 +0530

    0.

--- backend/Routes/routes.js
+++ backend/Routes/routes.js
@@ -1,11 +1,11 @@
 import express from "express";
-import { auth } from "../Middleware/index.js";
+import { auth } from "../Middleware/index.js"; 
 import {
   register,
   login,
   authenticated,
   createUser,
-} from "../Services/Auth.js";
+} from "../Services/Auth.js"; 
 
 import {
   createVideo,
@@ -14,20 +14,22 @@ import {
   getVideos,
   updateVideo,
   updateVideoStatus,
-} from "../Services/video.js";
+} from "../Services/video.js"; 
 
-const router = express.Router();
+const router = express.Router(); // Initializes Express router
 
-router.post("/register", register);
-router.post("/login", login);
-router.get("/auth", auth, authenticated);
-router.post("/create-user", createUser);
+// Authentication routes
+router.post("/register", register); // Route for user registration
+router.post("/login", login); // Route for user login
+router.get("/auth", auth, authenticated); // Route to check if user is authenticated (protected by `auth` middleware)
+router.post("/create-user", createUser); // Route to create new user
 
-router.post("/create-video", createVideo);
-router.get("/get-videos", auth, getVideos);
-router.get("/get-all-videos", getAllVideos);
-router.patch("/update-video", updateVideo);
-router.patch("/update-video-status", updateVideoStatus);
-router.delete("/delete-video", auth, deleteVideo);
+// Video management routes
+router.post("/create-video", createVideo); // Route to create new video
+router.get("/get-videos", auth, getVideos); // Route to get videos (protected by `auth` middleware)
+router.get("/get-all-videos", getAllVideos); // Route to get all videos without authentication
+router.patch("/update-video", updateVideo); // Route to update existing video
+router.patch("/update-video-status", updateVideoStatus); // Route to update status of a video
+router.delete("/delete-video", auth, deleteVideo); // Route to delete video (protected by `auth` middleware)
 
-export default router;
+export default router; 
